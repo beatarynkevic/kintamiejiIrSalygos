38959028a9e143a2cc19d573ea2a57ec23cbdf9c
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-02-08 20:42:00 +0200

    3 uzduotis

--- index.php
+++ index.php
@@ -15,16 +15,49 @@ echo '<br>';
 $pirmas = rand(0, 4);
 $antras = rand(0, 4);
 
-if($pirmas > $antras) {
+if($pirmas > $antras && $antras != 0) {
     echo "$pirmas pirmas skaicius, $antras antras skaicius";
     echo '<br> rezultatas:';
     echo round($pirmas/$antras, 2);
-} else {
+} else if($antras != 0 && $pirmas != 0){
     echo "$antras antras skaicius, $pirmas pirmas skaicius";
     echo '<br> rezultatas:';
     echo round($antras/$pirmas, 2);
 }
 
 //3 Uzduotis
+echo '<br>';
+echo '3 uzduotis';
+echo '<br>';
+$pirmasKintamasis = rand(0, 25);
+$antrasKintamasis = rand(0, 25);
+$treciasKintamasis = rand(0, 25);
+
+if($pirmasKintamasis > $antrasKintamasis && $pirmasKintamasis < $treciasKintamasis) {
+echo $pirmasKintamasis;
+} else if($pirmasKintamasis > $treciasKintamasis && $pirmasKintamasis < $antrasKintamasis) {
+    echo $pirmasKintamasis;
+} else if($antrasKintamasis > $pirmasKintamasis && $antrasKintamasis < $treciasKintamasis) {
+    echo $antrasKintamasis;
+} else if($antrasKintamasis > $treciasKintamasis && $antrasKintamasis < $pirmasKintamasis) {
+    echo $antras;
+} else if ($treciasKintamasis > $pirmasKintamasis && $treciasKintamasis < $antrasKintamasis) {
+    echo $treciasKintamasis;
+} else {
+    echo $treciasKintamasis;
+}
+
+//4 Uzduotis
+
+
+
+?>
+
+<?php
+
+is_int($name);
+is_int($name);
+is_int($name);
+is_int($name);
 
 ?>
