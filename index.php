cae92967374204f6a3db7f822585ce1ca5e40145
Beata Rynkevič <beata.rynkevic1@gmail.com>
2021-02-09 15:53:08 +0200

    5 uzduotys

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 //1 Uzduotis
+echo '1 uzduotis';
+echo '<br>';
 $name = 'Beata';
 $lastName = 'Rynkevic';
 $birthYear = 1995;
@@ -9,9 +11,13 @@ $currentAge = $currentYear - $birthYear;
 
 $answer = "As esu $name $lastName. Man yra $currentAge metai";
 echo $answer;
+echo '<br>';
+echo '<br>';
 
 //2 Uzduotis
+echo '2 uzduotis';
 echo '<br>';
+
 $pirmas = rand(0, 4);
 $antras = rand(0, 4);
 
@@ -23,10 +29,15 @@ if($pirmas > $antras && $antras != 0) {
     echo "$antras antras skaicius, $pirmas pirmas skaicius";
     echo '<br> rezultatas:';
     echo round($antras/$pirmas, 2);
+} else {
+    echo '<br>';
+    echo 'Pagavau nuli';
 }
 
-//3 Uzduotis
 echo '<br>';
+echo '<br>';
+
+//3 Uzduotis
 echo '3 uzduotis';
 echo '<br>';
 $pirmasKintamasis = rand(0, 25);
@@ -34,20 +45,63 @@ $antrasKintamasis = rand(0, 25);
 $treciasKintamasis = rand(0, 25);
 
 if($pirmasKintamasis > $antrasKintamasis && $pirmasKintamasis < $treciasKintamasis) {
-echo $pirmasKintamasis;
+echo "Vidurine reiksme yra: $pirmasKintamasis";
 } else if($pirmasKintamasis > $treciasKintamasis && $pirmasKintamasis < $antrasKintamasis) {
-    echo $pirmasKintamasis;
+    echo "Vidurine reiksme yra: $pirmasKintamasis";
 } else if($antrasKintamasis > $pirmasKintamasis && $antrasKintamasis < $treciasKintamasis) {
-    echo $antrasKintamasis;
+    echo "Vidurine reiksme yra: $antrasKintamasis";
 } else if($antrasKintamasis > $treciasKintamasis && $antrasKintamasis < $pirmasKintamasis) {
-    echo $antras;
+    echo "Vidurine reiksme yra: $antrasKintamasis";
 } else if ($treciasKintamasis > $pirmasKintamasis && $treciasKintamasis < $antrasKintamasis) {
-    echo $treciasKintamasis;
+    echo "Vidurine reiksme yra: $treciasKintamasis";
 } else {
-    echo $treciasKintamasis;
+    echo "Vidurine reiksme yra: $treciasKintamasis";
 }
+echo '<br>';
 
 //4 Uzduotis
+echo '<br>';
+echo '4 uzduotis';
+echo '<br>';
+
+$a = rand(1, 10);
+$b = rand(1, 10);
+$c = rand(1, 10);
+
+if( ($a+$b)>$c && ($a+$c)>$b && ($b+$c)>$a ) {
+    echo 'Susidaro trikampis is: '."($a + $b + $c)";
+} else{
+    echo 'Nesusidaro trikampis';
+}
+
+//5 Uzduotis
+echo '<br><br>';
+echo '5 uzduotis';
+echo '<br>';
+
+$first = rand(0, 2);
+$second = rand(0, 2);
+$third = rand(0, 2);
+$fourth = rand(0, 2);
+
+$countZero = 0;
+$countOne = 0;
+$countTwo = 0;
+
+if ($first === 0) $countZero++;
+if ($second === 0) $countZero++;
+if ($third === 0) $countZero++;
+if ($fourth === 0) $countZero++;
+if ($first === 1) $countOne++;
+if ($second === 1) $countOne++;
+if ($third === 1) $countOne++;
+if ($fourth === 1) $countOne++;
+if ($first === 2) $countTwo++;
+if ($second === 2) $countTwo++;
+if ($third === 2) $countTwo++;
+if ($fourth === 2) $countTwo++;
+
+echo 'nuliu yra:'.$countZero.'<br>'.'vienetu yra: '.$countOne.'<br>'.'dviejetu yra: '.$countTwo.'<br>';
 
 
 
